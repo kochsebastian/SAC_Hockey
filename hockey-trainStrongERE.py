ae2085903653425c16b5e3c6a29a45ea32760632
Sebastian Koch <koch.sebastian@mail.com>
2021-03-12 09:23:29 +0100

    minor traning changes

--- hockey-trainStrongERE.py
+++ hockey-trainStrongERE.py
@@ -23,7 +23,7 @@ parser.add_argument('--policy', default="Gaussian",
                     help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
 parser.add_argument('--eval', type=bool, default=True,
                     help='Evaluates a policy a policy every 10 episode (default: True)')
-parser.add_argument('--gamma', type=float, default=0.97, metavar='G',
+parser.add_argument('--gamma', type=float, default=0.95, metavar='G',
                     help='discount factor for reward (default: 0.99)')
 parser.add_argument('--tau', type=float, default=0.005, metavar='G',
                     help='target smoothing coefficient(τ) (default: 0.005)')
@@ -31,7 +31,7 @@ parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
                     help='learning rate (default: 0.0003)')
 parser.add_argument('--wd', type=float, default=0.0, metavar='G',
                     help='learning rate (default: 0.0)')
-parser.add_argument('--alpha', type=float, default=0.1, metavar='G',
+parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
                     help='Temperature parameter α determines the relative importance of the entropy\
                             term against the reward (default: 0.2)')
 parser.add_argument('--automatic_entropy_tuning', type=bool, default=True, metavar='G',
@@ -40,9 +40,9 @@ parser.add_argument('--seed', type=int, default=123456, metavar='N',
                     help='random seed (default: 123456)')
 parser.add_argument('--batch_size', type=int, default=4, metavar='N',
                     help='batch size (default: 256)')
-parser.add_argument('--num_steps', type=int, default=10000001, metavar='N',
+parser.add_argument('--num_steps', type=int, default=3000001, metavar='N',
                     help='maximum number of steps (default: 1000000)')
-parser.add_argument('--hidden_size', type=int, default=512, metavar='N',
+parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
                     help='hidden size (default: 256)')
 parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
                     help='model updates per simulator step (default: 1)')
@@ -50,7 +50,7 @@ parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
                     help='Steps sampling random actions (default: 10000)')
 parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
                     help='Value target update per no. of updates per step (default: 1)')
-parser.add_argument('--replay_size', type=int, default=100000000, metavar='N',
+parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
                     help='size of replay buffer (default: 10000000)')
 parser.add_argument('--cuda', action="store_true",
                     help='run on CUDA (default: False)')
@@ -62,18 +62,16 @@ args = parser.parse_args()
 env = h_env.HockeyEnv(mode=h_env.HockeyEnv.NORMAL)
 # Agent
 agent = SAC(env.observation_space.shape[0], env.action_space, args)
-actor = "full_player_models/sac_actor_hockey_reward-8.938693169580354_episode-4500_batch_size-4_gamma-0.95_tau-0.005_lr-0.0003_alpha-0.2_tuning-True_hidden_size-512_updatesStep-1_startSteps-10000_targetIntervall-1_replaysize-1000000_t-2021-03-10_23-26-27"
-critic = "full_player_models/sac_critic_hockey_reward-8.938693169580354_episode-4500_batch_size-4_gamma-0.95_tau-0.005_lr-0.0003_alpha-0.2_tuning-True_hidden_size-512_updatesStep-1_startSteps-10000_targetIntervall-1_replaysize-1000000_t-2021-03-10_23-26-27"
-
-agent.load_model(actor,critic)
+agent.load_model('full_player_models/sac_actor_hockey_11200_batch_size-4_gamma-0.95_tau-0.005_lr-0.0003_alpha-0.2_tuning-True_hidden_size-256_updatesStep-1_startSteps-10000_targetIntervall-1_replaysize-1000000','full_player_models/sac_critic_hockey_11200_batch_size-4_gamma-0.95_tau-0.005_lr-0.0003_alpha-0.2_tuning-True_hidden_size-256_updatesStep-1_startSteps-10000_targetIntervall-1_replaysize-1000000')
 # opponent = copy.deepcopy(agent)
-opponent = h_env.BasicOpponent(weak=False)
+basic_strong = h_env.BasicOpponent(weak=False)
 time_ = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 #Tesnorboard
+writer = SummaryWriter(f"strongplay-runs/ERE{time_}_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}")
 
 # Memory
-# memory = PrioritizedReplay(args.replay_size)
-memory = ReplayMemory(args.replay_size,args.seed)
+memory = ERE_PrioritizedReplay(args.replay_size)
+# memory = ReplayMemory(args.replay_size,args.seed)
 
 # Training Loop
 total_numsteps = 0
@@ -82,8 +80,11 @@ updates = 0
 
 o = env.reset()
 # _ = env.render()
-writer = SummaryWriter(f"strongplay-runs-replay/{time_}_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}")
 
+eta_0 = 0.996
+eta_T = 1.0
+max_ep_len = 1000 
+c_k_min = 2500 # original = 5000
 for i_episode in itertools.count(1):
     episode_reward = 0
     episode_steps = 0
@@ -97,23 +98,24 @@ for i_episode in itertools.count(1):
         else:
             action = agent.select_action(state)  # Sample action from policy
 
-        if len(memory) > args.batch_size:
-            # Number of updates per step in environment
-            for i in range(args.updates_per_step):
-                # Update parameters of all the networks
-                critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha, memory_ = agent.update_parameters(memory, args.batch_size, updates)
-                memory=memory_
+        # if len(memory) > args.batch_size:
+        #     # Number of updates per step in environment
+        #     for i in range(args.updates_per_step):
+        #         # Update parameters of all the networks
+        #         critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
+        #         # memory=memory_
 
-                writer.add_scalar('loss/critic_1', critic_1_loss, updates)
-                writer.add_scalar('loss/critic_2', critic_2_loss, updates)
-                writer.add_scalar('loss/policy', policy_loss, updates)
-                writer.add_scalar('loss/entropy_loss', ent_loss, updates)
-                writer.add_scalar('entropy_temprature/alpha', alpha, updates)
-                updates += 1
+        #         writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+        #         writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+        #         writer.add_scalar('loss/policy', policy_loss, updates)
+        #         writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+        #         writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+        #         updates += 1
 
         # a2 = [10,0.,0,0] 
         obs_agent2 = env.obs_agent_two()
-        a2 = opponent.act(obs_agent2)
+        
+        a2 = basic_strong.act(obs_agent2)
         next_state, reward, done, _ = env.step(np.hstack([action[0:4],a2[0:4]])) 
         
         # env.render()
@@ -128,12 +130,29 @@ for i_episode in itertools.count(1):
         # mask = float(not done)
         # memory.append(state,action,reward,next_state,mask,episode_done=done)
         # memory.push(state, action, reward, next_state, mask) # Append transition to memory
+        # memory.push(state, action, reward, next_state, mask)
         memory.push(state, action, reward, next_state, mask)
+        eta_t = eta_0 + (eta_T - eta_0)*(total_numsteps/args.num_steps)
 
         state = next_state
 
     if total_numsteps > args.num_steps:
         break
+
+    for k in range(1,episode_steps):
+        c_k = max(int(len(memory)*eta_t**(k*(251/episode_steps))), c_k_min)
+        for i in range(args.updates_per_step):
+            # Update parameters of all the networks
+            critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha, memory_ = agent.update_parameters(memory, args.batch_size, updates,c_k=c_k)
+            memory=memory_
+
+            writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+            writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+            writer.add_scalar('loss/policy', policy_loss, updates)
+            writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+            writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+            updates += 1
+
 
     writer.add_scalar('reward/train', episode_reward, i_episode)
     print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps, episode_steps, round(episode_reward, 2)))
@@ -141,7 +160,7 @@ for i_episode in itertools.count(1):
     if i_episode % 10 == 0 and args.eval is True:
         avg_reward = 0.
         episodes = 5
-        for _  in range(episodes):
+        for k  in range(episodes):
             state = env.reset()
             episode_reward = 0
             done = False
@@ -149,7 +168,8 @@ for i_episode in itertools.count(1):
                 
                 action = agent.select_action(state, evaluate=True)
                 obs_agent2 = env.obs_agent_two()
-                a2 = opponent.act(obs_agent2)
+
+                a2 = basic_strong.act(obs_agent2)
                 next_state, reward, done, _ = env.step(np.hstack([action[0:4],a2[0:4]])) 
                 # env.render()
                 episode_reward += reward
@@ -161,10 +181,10 @@ for i_episode in itertools.count(1):
 
         if i_episode%500==0:
             time_ = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            agent.save_model( "strongplay_models_replay", "hockeyStrongPRE", suffix=f"reward-{avg_reward}_episode-"+str(i_episode)+f"_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}_t-{time_}")
-        # if i_episode%5000==0:
-        #     opponent.policy.load_state_dict(agent.policy.state_dict())
-        #     opponent.critic.load_state_dict(agent.critic.state_dict())
+            agent.save_model( "strongplay_models", "hockeyStrongERE", suffix=f"reward-{avg_reward}_episode-"+str(i_episode)+f"_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}_t-{time_}")
+        if i_episode%5000==0:
+            opponent.policy.load_state_dict(agent.policy.state_dict())
+            opponent.critic.load_state_dict(agent.critic.state_dict())
         writer.add_scalar('avg_reward/test', avg_reward, i_episode)
 
         print("----------------------------------------")
