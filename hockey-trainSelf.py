e2ae8733a49598c332822c2fb07a82859763de40
Sebastian Koch <koch.sebastian@mail.com>
2021-03-13 13:43:52 +0100

    tensorboard csvs

--- hockey-trainSelf.py
+++ hockey-trainSelf.py
@@ -37,7 +37,7 @@ parser.add_argument('--seed', type=int, default=123456, metavar='N',
                     help='random seed (default: 123456)')
 parser.add_argument('--batch_size', type=int, default=8, metavar='N',
                     help='batch size (default: 256)')
-parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
+parser.add_argument('--num_steps', type=int, default=30000001, metavar='N',
                     help='maximum number of steps (default: 1000000)')
 parser.add_argument('--hidden_size', type=int, default=512, metavar='N',
                     help='hidden size (default: 256)')
@@ -45,7 +45,7 @@ parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
                     help='model updates per simulator step (default: 1)')
 parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
                     help='Steps sampling random actions (default: 10000)')
-parser.add_argument('--target_update_interval', type=int, default=5, metavar='N',
+parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
                     help='Value target update per no. of updates per step (default: 1)')
 parser.add_argument('--replay_size', type=int, default=10000000, metavar='N',
                     help='size of replay buffer (default: 10000000)')
@@ -66,7 +66,7 @@ opponent = copy.deepcopy(agent)
 basic_strong = h_env.BasicOpponent(weak=False)
 time_ = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 #Tesnorboard
-writer = SummaryWriter(f"selfplay-final-runs/5000updates{time_}_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}")
+writer = SummaryWriter(f"selfplay-final-runs/1000updates{time_}_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}")
 
 # Memory
 # memory = PrioritizedReplay(args.replay_size)
@@ -79,7 +79,7 @@ updates = 0
 
 o = env.reset()
 # _ = env.render()
-
+last_avg=0
 for i_episode in itertools.count(1):
     episode_reward = 0
     episode_steps = 0
@@ -109,11 +109,12 @@ for i_episode in itertools.count(1):
 
         # a2 = [10,0.,0,0] 
         obs_agent2 = env.obs_agent_two()
-        if i_episode % 2 != 0:
+        if i_episode % 3 != 0: # more selfplay
             a2 = opponent.select_action(obs_agent2, evaluate=True)
         else:
             a2 = basic_strong.act(obs_agent2)
         next_state, reward, done, _ = env.step(np.hstack([action[0:4],a2[0:4]])) 
+        # env.render()
         
         # env.render()
         episode_steps += 1
@@ -134,12 +135,12 @@ for i_episode in itertools.count(1):
     if total_numsteps > args.num_steps:
         break
 
-    writer.add_scalar('reward/train', episode_reward, i_episode)
+    # writer.add_scalar('reward/train', episode_reward, i_episode)
     print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps, episode_steps, round(episode_reward, 2)))
 
-    if i_episode % 10 == 0:
+    if i_episode % 13 == 0:
         avg_reward = 0.
-        episodes = 6
+        episodes = 9
         for k  in range(episodes):
             state = env.reset()
             episode_reward = 0
@@ -148,7 +149,7 @@ for i_episode in itertools.count(1):
                 
                 action = agent.select_action(state, evaluate=True)
                 obs_agent2 = env.obs_agent_two()
-                if k < 4: 
+                if k % 3 != 0: # more selfplay
                     a2 = opponent.select_action(obs_agent2, evaluate=True)
                 else:
                     a2 = basic_strong.act(obs_agent2)
@@ -160,19 +161,20 @@ for i_episode in itertools.count(1):
                 state = next_state
             avg_reward += episode_reward
         avg_reward /= episodes
+        last_avg = avg_reward
 
-        if i_episode%500==0:
-            time_ = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            agent.save_model( "final-models", "5000updates_hockey", suffix=f"reward-{avg_reward}_episode-"+str(i_episode)+f"_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}_t-{time_}")
-        if i_episode%5000==0:
-            opponent.policy.load_state_dict(agent.policy.state_dict())
-            opponent.critic.load_state_dict(agent.critic.state_dict())
         writer.add_scalar('avg_reward/test', avg_reward, i_episode)
 
         print("----------------------------------------")
         print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
         print("----------------------------------------")
-
+    if i_episode%500==0:
+        time_ = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        agent.save_model( "final-models", "1000updates_hockey512_t1", suffix=f"reward-{last_avg}_episode-"+str(i_episode)+f"_batch_size-{args.batch_size}_gamma-{args.gamma}_tau-{args.tau}_lr-{args.lr}_alpha-{args.alpha}_tuning-{args.automatic_entropy_tuning}_hidden_size-{args.hidden_size}_updatesStep-{args.updates_per_step}_startSteps-{args.start_steps}_targetIntervall-{args.target_update_interval}_replaysize-{args.replay_size}_t-{time_}")
+    if i_episode%1000==0:
+        opponent.policy.load_state_dict(agent.policy.state_dict())
+        opponent.critic.load_state_dict(agent.critic.state_dict())
+        opponent.critic_target.load_state_dict(agent.critic_target.state_dict())
 env.close()
 
 
